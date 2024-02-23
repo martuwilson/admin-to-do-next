ea724acd674e05860bd1bbbe156e13a88c0c68e8
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-02-23 15:52:40 -0300

    check 1st route with postman

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+
+  return NextResponse.json({ message: 'Hello, World!' })
+}
+
+export async function POST(request: Request) { 
+
+    return NextResponse.json({ message: 'Hey POST!', method: 'POST'})
+  }
