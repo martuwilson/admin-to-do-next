9fded923ea7a2e96da32f88b5d4b14e3603e642e
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-02-24 01:10:10 -0300

    simple pagination added

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,9 +3,24 @@
 import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET(request: Request) { 
+export async function GET(request: Request) {
 
-    const todos = await prisma.todo.findMany();
+    const {searchParams} = new URL(request.url);
+    const take = searchParams.get('take') ?? '10';
+    const skip = searchParams.get('skip') ?? '0';
+
+    if(isNaN(+ take)) {
+        return NextResponse.json({error: 'Take debe ser un numero'}, {status: 400});
+    }
+
+    if(isNaN(+ skip)) {
+        return NextResponse.json({error: 'Skip debe ser un numero'}, {status: 400});
+    }
+
+    const todos = await prisma.todo.findMany({
+        take: +take,
+        skip: +skip,
+    });
 
   return NextResponse.json(todos);
 }
