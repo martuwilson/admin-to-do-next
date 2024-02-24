2415159dec7ae4c3727fabce7b3ab2401f2f8c6b
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-02-24 13:28:44 -0300

    yup to validate schemas added

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -28,12 +29,24 @@ export async function GET(request: Request) {
   return NextResponse.json(todos);
 }
 
+const postSchema = yup.object({
+  description: yup.string().required(),
+  completed: yup.boolean().optional().default(false),
+});
+
 export async function POST(request: Request) {
-  const body = await request.json();
+  try {
+    const {completed, description} = await postSchema.validate(await request.json());
 
-  const todo = await prisma.todo.create({
-    data: body,
-  });
+    const todo = await prisma.todo.create({
+      data: {
+        description,
+        completed,
+      },
+    });
 
-  return NextResponse.json(todo);
+    return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json(error, {status: 400});
+  }
 }
