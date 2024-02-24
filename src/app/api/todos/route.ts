b9de4bac69bbf8abca455240342f07febc368e8f
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-02-24 01:03:09 -0300

    seed data added && get all data

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,11 @@
+
+
+import prisma from '@/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+
+    const todos = await prisma.todo.findMany();
+
+  return NextResponse.json(todos);
+}
