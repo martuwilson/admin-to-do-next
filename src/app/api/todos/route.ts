b82aa57fbb003d98cefd08a1109e8af8955d40df
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-02-24 13:19:39 -0300

    POST method added

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,26 +1,39 @@
-
-
-import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const take = searchParams.get("take") ?? "10";
+  const skip = searchParams.get("skip") ?? "0";
 
-    const {searchParams} = new URL(request.url);
-    const take = searchParams.get('take') ?? '10';
-    const skip = searchParams.get('skip') ?? '0';
+  if (isNaN(+take)) {
+    return NextResponse.json(
+      { error: "Take debe ser un numero" },
+      { status: 400 }
+    );
+  }
 
-    if(isNaN(+ take)) {
-        return NextResponse.json({error: 'Take debe ser un numero'}, {status: 400});
-    }
+  if (isNaN(+skip)) {
+    return NextResponse.json(
+      { error: "Skip debe ser un numero" },
+      { status: 400 }
+    );
+  }
 
-    if(isNaN(+ skip)) {
-        return NextResponse.json({error: 'Skip debe ser un numero'}, {status: 400});
-    }
-
-    const todos = await prisma.todo.findMany({
-        take: +take,
-        skip: +skip,
-    });
+  const todos = await prisma.todo.findMany({
+    take: +take,
+    skip: +skip,
+  });
 
   return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const todo = await prisma.todo.create({
+    data: body,
+  });
+
+  return NextResponse.json(todo);
 }
